cfc130db5fba368b11b0d95c77469a2dcd2aacac
R Boelter <ronny.boelter@outlook.com>
2021-02-16 15:13:06 +0100

    cypress fix

--- cypress/tests/functional/Citations.spec.js
+++ cypress/tests/functional/Citations.spec.js
@@ -3,7 +3,8 @@ describe('Scopus/Crossref Plugin tests', function () {
 
 	it('Disable Scopus/Crossref Plugin', function () {
 		cy.login('admin', 'admin', 'publicknowledge');
-		cy.get('nav[class="app__nav"] a:contains("Website")').click();
+		cy.get('ul[id="navigationPrimary"] a:contains("Settings")').click();
+		cy.get('ul[id="navigationPrimary"] a:contains("Website")').click();
 		cy.get('button[id="plugins-button"]').click();
 		// disable plugin if enabled
 		cy.get('input[id^="select-cell-citationsplugin-enabled"]')
@@ -18,7 +19,8 @@ describe('Scopus/Crossref Plugin tests', function () {
 
 	it('Enable Scopus/Crossref Plugin', function () {
 		cy.login('admin', 'admin', 'publicknowledge');
-		cy.get('nav[class="app__nav"] a:contains("Website")').click();
+		cy.get('ul[id="navigationPrimary"] a:contains("Settings")').click();
+		cy.get('ul[id="navigationPrimary"] a:contains("Website")').click();
 		cy.get('button[id="plugins-button"]').click();
 		// Find and enable the plugin
 		cy.get('input[id^="select-cell-citationsplugin-enabled"]').click();
