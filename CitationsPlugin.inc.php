588a47a970af019308823d802780aa07600da375
Jhon <jhon@lepidus.com.br>
2021-08-12 16:02:55 -0400

    Adds support for showing citations in OPS
    Signed-off-by: Lais <lais@lepidus.com.br>
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- CitationsPlugin.inc.php
+++ CitationsPlugin.inc.php
@@ -32,19 +32,35 @@ class CitationsPlugin extends GenericPlugin
 				$request->getBaseUrl() . '/' . $this->getPluginPath() . '/css/citations.css'
 			);
 			HookRegistry::register('Templates::Article::Details', array($this, 'citationsContent'));
+			HookRegistry::register('Templates::Preprint::Details', array($this, 'citationsContent'));
 			HookRegistry::register('LoadHandler', array($this, 'setPageHandler'));
 		}
 		return $success;
 	}
 
+	private function getPubId($smarty) {
+		$application = Application::getName();
+		$pubId = null;
+		switch($application){
+			case 'ojs2':
+				$article = $smarty->getTemplateVars('article');
+				$pubId = $article->getStoredPubId('doi');
+				break;
+			case 'ops':
+				$preprint = $smarty->getTemplateVars('preprint');
+				$pubId = $preprint->getStoredPubId('doi');
+				break;
+		}
+
+		return $pubId;
+	}
 
 	public function citationsContent($hookName, $args)
 	{
 		$request = Application::get()->getRequest();
 		$smarty =& $args[1];
 		$output =& $args[2];
-		$article = $smarty->getTemplateVars('article');
-		$pubId = $article->getStoredPubId('doi');
+		$pubId = $this->getPubId($smarty);
 		$contextId = $request->getContext()->getId();
 		$settings = json_decode($this->getSetting($contextId, 'settings'), true);
 		if ($pubId != null && $pubId != '' && $settings) {
